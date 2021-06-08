98f41f27bb02f727b0bec12e2003b31268bd73bf
Théo <theomarion08@gmail.com>
2021-06-09 00:57:29 +0200

    6.1 bdd.sql maj

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 03 juin 2021 à 10:32
+-- Généré le : mar. 08 juin 2021 à 22:56
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -35,34 +35,7 @@ CREATE TABLE IF NOT EXISTS `favori` (
   `id_utilisateur` int(11) NOT NULL,
   PRIMARY KEY (`numero`),
   KEY `favori_utilisateur_FK` (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `filtre`
---
-
-DROP TABLE IF EXISTS `filtre`;
-CREATE TABLE IF NOT EXISTS `filtre` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(50) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `souscrit`
---
-
-DROP TABLE IF EXISTS `souscrit`;
-CREATE TABLE IF NOT EXISTS `souscrit` (
-  `Id` int(11) NOT NULL,
-  `Numero` int(11) NOT NULL,
-  PRIMARY KEY (`Id`,`Numero`),
-  KEY `souscrit_Utilisateur0_FK` (`Numero`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -80,14 +53,15 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Mdp` varchar(50) NOT NULL,
   `Adm` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_utilisateur`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id_utilisateur`, `Nom`, `Prenom`, `Mail`, `Adresse_postale`, `Mdp`, `Adm`) VALUES
-(1, 'MARION', 'ThÃ©o', 'theomarion08@gmail.com', '6 rue des bigo', '7682fe272099ea26efe39c890b33675b', 1);
+(1, 'MARION', 'ThÃ©o', 'theomarion08@gmail.com', '6 rue des bigos', '7682fe272099ea26efe39c890b33675b', 1),
+(2, 'MALAMAIRE', 'Hugo', 'hugo@gmail.com', '10 rue du docteur', 'azdad', 0);
 
 --
 -- Contraintes pour les tables déchargées
@@ -98,13 +72,6 @@ INSERT INTO `utilisateur` (`id_utilisateur`, `Nom`, `Prenom`, `Mail`, `Adresse_p
 --
 ALTER TABLE `favori`
   ADD CONSTRAINT `favori_utilisateur_FK` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
-
---
--- Contraintes pour la table `souscrit`
---
-ALTER TABLE `souscrit`
-  ADD CONSTRAINT `souscrit_Filtre_FK` FOREIGN KEY (`Id`) REFERENCES `filtre` (`Id`),
-  ADD CONSTRAINT `souscrit_Utilisateur0_FK` FOREIGN KEY (`Numero`) REFERENCES `utilisateur` (`id_utilisateur`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
